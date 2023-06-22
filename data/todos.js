0afa088a5b2a9817004b4446ec084a95bc29cb5d
Lucas García <lucas@codeccoop.org>
2023-06-22 02:50:52 +0200

    feat: change todos

--- data/todos.js
+++ data/todos.js
@@ -2,7 +2,7 @@ const data = {
   "Ser soci d'una cooperativa": true,
   "Haver fet l'aportació de capital corresponent": true,
   "Complir amb les teves obligacions": true,
-  "Cobrar menys del que cobraries en una altra banda": true,
+  "Participar activament a les assemblees": true,
 };
 
 module.exports = data;
